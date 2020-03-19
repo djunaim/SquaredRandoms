89e6fd1094204118700176f3e62662365ab19663
Monica Djunaidi <modjun12@gmail.com>
2020-03-18 20:53:52 -0500

    Add project files.

---
+++ SquaredRandoms/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SquaredRandoms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> squaredNumsList = new List<int> { };
+
+            var randomNums = new RandomNum();
+            foreach (var randomNum in randomNums.RandomNumbersList)
+            {
+                Console.WriteLine(randomNum);
+            }
+
+            foreach (var randomNum in randomNums.RandomNumbersList)
+            {
+                var squaredNums = randomNum * randomNum;
+                squaredNumsList.Add(squaredNums);
+                Console.WriteLine(squaredNums);
+            }
+
+            foreach (var evenNum in squaredNumsList)
+            {
+                if (evenNum % 2 == 0)
+                {
+                    Console.WriteLine($"This is even numbers: {evenNum}");
+                }
+            }
+        }
+    }
+}
---
+++ SquaredRandoms/RandomNum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SquaredRandoms
+{
+    class RandomNum
+    {
+        public List<int> RandomNumbersList = new List<int> { };
+        public RandomNum()
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                RandomNumbersList.Add(rnd.Next(1, 51));
+            }
+        }
+    }
+}
